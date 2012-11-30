c0ccac0bd84264623bf6651b64fb2e0236de1d98
Daiwei Lu <daiweilu123@gmail.com>
2012-11-29 19:34:37 -0600

    Update index.html template

--- README.md
+++ README.md
@@ -2,3 +2,5 @@ ES-homewrok
 ===========
 
 The purpose of this git repo is to finish the ES homework
+
+Due on December 11, 2012 2:00pm
--- _js/main.js
+++ _js/main.js
@@ -1,1 +1,6 @@
-
+$('#search-input').focus(function () {
+	$(this).animate({'width': 680}, 400);
+})
+.blur(function () {
+	$(this).animate({'width': 360}, 400);
+});
